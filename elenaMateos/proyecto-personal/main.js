aefc3e41ec58e459f5529c28842cce1c2078ab56
ElenaML <elena.mateos1@gmail.com>
2017-11-04 23:44:11 +0100

    Starting proyect

---
+++ elenaMateos/proyecto-personal/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+function peticionAjax(url) {
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.onreadystatechange = function() {
+
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            console.info(JSON.parse(xmlHttp.responseText));
+        } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
+            console.error("ERROR! 404");
+            console.info(JSON.parse(xmlHttp.responseText));
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+
+peticionAjax("http://airemadrid.herokuapp.com/api/estacion/99");
+
+*/
