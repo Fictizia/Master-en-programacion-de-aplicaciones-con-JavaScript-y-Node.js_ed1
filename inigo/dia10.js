e7bab1b33d8ad79dd2f1189bf6d67845b6df4c22
Inigo <inigojorcano@gmail.com>
2017-11-14 10:42:24 +0000

    update ej10 fecha

--- inigo/dia10.js
+++ inigo/dia10.js
@@ -31,3 +31,61 @@
 		console.log("Veces que suman 10: " + con);
 
 //1 - Diseña un script que confirme si una fecha es valida y además devuelva la fecha en dos formatos diferentes.
+
+        var fecha = function fecha(){
+            
+            var dia = prompt("Introduce el día");
+            while(true) {
+                if(isNaN(dia)){
+                    dia = prompt("Debe introducir un número para el dia");
+                }else{
+                    if(dia > 0 && dia <= 31){
+                        break;
+                    }else{
+                        dia = prompt("Debe introducir un número entre el 1 y 31 incluidos");
+                    }
+                }            
+            }
+            
+            var mes = prompt("Introduce el mes");
+            while(true) {
+                if(isNaN(mes)){
+                    mes = prompt("Debe introducir un número para el mes");
+                }else{
+                    if(mes > 0 && mes <= 12){
+                        break;
+                    }else{
+                        mes = prompt("Debe introducir un número entre el 1 y 12 incluidos");
+                    }
+                }     
+            }
+            
+            var anio = prompt("Introduce el año");
+            while(true) {
+                if(isNaN(anio)){
+                    anio = prompt("Debe introducir un número para el anio");
+                }else{
+                    if(anio > 1979 && anio <= 2050){
+                        break;
+                    }else{
+                        anio = prompt("Debe introducir un número entre el 1980 y 2050 incluidos");
+                    }
+                }
+            }
+            var fecha = new Date(anio, mes - 1, dia);
+            return fecha;
+        }();
+        
+        var opciones1 = { 
+            year: 'numeric', 
+            month: 'numeric', 
+            day: 'numeric'
+        };
+        var opciones2 = { 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric'
+        };
+            
+        console.log("Formato 1: " + fecha.toLocaleString("es-ES",opciones1));
+        console.log("Formato 2: " + fecha.toLocaleString("es-ES",opciones2));
