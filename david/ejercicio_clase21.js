e12912be9ae95fbdecf191c1aadfd7366daf78bf
k0sasp <davidsalazarsalinas@gmail.com>
2017-11-29 10:47:37 +0000

    loadChuli

--- david/ejercicio_clase21.js
+++ david/ejercicio_clase21.js
@@ -1,11 +1,14 @@
 function peticionAjax(url) {
         var xmlHttp = new XMLHttpRequest();
-
+        
         xmlHttp.onreadystatechange = function() {
+            if(!document.querySelector('.loading')){
+                document.querySelector('body').innerHTML += "<div class='loading'>"+"<img src='loader.svg'/>"+"</div>"
+            }
 
             if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
                 var datos = JSON.parse(xmlHttp.responseText);
-                console.log(sacarDatos(datos));
+                    sacarDatos(datos);
             } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
                 console.error("ERROR! 404");
             }
@@ -16,32 +19,31 @@ function peticionAjax(url) {
 
 peticionAjax("https://api.nytimes.com/svc/books/v3/lists.json?api-key=4f6641e4884e41c387cf36d7f535b0b7&list=hardcover-fiction&sort-order=ASC")
 
-var objetoDatos = {
-    urlAmazon: '',
-    rankingPos: 0,
-    titulo: '',
-    semanas: 0,
-    descricion: '',
-    caratula: ''
-}
-
 function sacarDatos(datos){
     var results = datos.results
+    var html = '';
     
-    results.forEach(function(e){
-        var objetito = new objetoDatos();
-        objetito.urlAmazon = e.amazon_product_url
-        objetito.rankingPos = e.rank
-        objetito.titulo = e.book_deatils[0].title
-        objetito.semanas = e.rank_last_week
-        objetito.descricion = e.book_deatils[0].description
-        objetito.caratula = e.isbns[0].isbn13
-        
-        return objetito;
+    results.forEach(function(e,i){
+        setTimeout(function(){
+            html += 
+            "<div class='box-content'>"+
+                "<div class='textos'>"+
+                    "<h3 class='semanasLista'>"+e.weeks_on_list+" semanas en lista"+"</h3>"+
+                    "<h2 class='titulo'>"+"#"+e.rank+" ---- "+e.book_details[0].title+"</h2>"+
+                    "<p>"+e.book_details[0].description+"</p>"+
+                    "<a href="+e.amazon_product_url+" target='_blank'>Amazon</a>"+
+                "</div>"+
+                "<div class='imagen'>"+
+                    "<img src='https://s1.nyt.com/du/books/images/" + e.book_details[0].primary_isbn13 + ".jpg'/>"
+                +"</div>"
+            +"</div>"
+            document.querySelector(".container").innerHTML = html;
+            if(i>=results.length-1){
+                document.querySelector('.loading').remove();
+            }
+        }, i*1000)
+        /*setTimeout(function(){
+            document.querySelector('.loading').remove();
+        },results.length*1000);*/
     });
-}
-
-
-function imprimir(objeto){
-    
 }
