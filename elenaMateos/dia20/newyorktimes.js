e7ba1a9da6e7fb72759adefec63fa666056e401f
ElenaML <elena.mateos1@gmail.com>
2017-12-01 23:03:25 +0100

    Refactored code

--- elenaMateos/dia20/newyorktimes.js
+++ elenaMateos/dia20/newyorktimes.js
@@ -1,18 +1,6 @@
-// TOKEN:   8ffd1ba3a80c41e8bc7eaf0b17215c5a
+
 var token = "217027b37588437fbc3efb9984aca1a6";
 var url = "https://api.nytimes.com/svc/books/v3/lists.json?api-key="+token+"&list=hardcover-fiction&sort-order=ASC";
-
-// var container = document.getElementById('container');
-// var bookCard = document.getElementById('bookCard');
-// var title = document.getElementById('title');
-// var pic = document.getElementById('pic');
-// var autor = document.getElementById('autor');
-// var amazon = document.getElementById('amazon');
-// var weeks = document.getElementById('weeks');
-// var listNunmber = '';
-// var i = 0;
-
-
 
 
 function askForBooks() {
@@ -57,28 +45,5 @@ container.innerHTML +='<ul class="book-card">' +
                       ' <li><a id="amazon" class="list-element" href="' + results[i].amazon_product_url+ '">Comprar en Amazon...</a></li>' +
                       '</ul>';
 
-    /*
-    <ul class="book-card">
-      <li  class="list-element"></li>
-      <li id="" class="list-element"><img id="pic" src="" class="book-pic"></img ></li>
-      <li id="weeks" class="list-element"></li>
-      <li id="autor" class="list-element"></li>
-      <li><a id="amazon" class="list-element" href="">Comprar en Amazon...</a></li>
-    </ul>
-
-    amazon.href = results[i].amazon_product_url;
-    title.innerHTML = '#' + (i+1) + '. Título: ' + results[i].book_details[0].title;
-    pic.src = 'https://s1.nyt.com/du/books/images/' + results[i].isbns[0].isbn13 + '.jpg';
-    autor.innerHTML = results[i].book_details[0].author;
-    weeks.innerHTML = results[i].weeks_on_list + 'Weeks on List';
-    */
   }
 }
-
-
-
-/*
-for (var i = 0, i<results.length; i++; ) {
-    amazon.innerHTML.src = results[i].amazon_product_url;
-}
-*/
