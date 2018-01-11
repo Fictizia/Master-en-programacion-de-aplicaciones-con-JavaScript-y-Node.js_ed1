64c9284e8ac7edcb3fda7575d0a107dbd5cc1e53
Victor Martin Rodriguez <vmrodriguez1995@gmail.com>
2018-01-11 16:17:17 +0000

    enlace meme

--- victor/memeneitor/js/app.js
+++ victor/memeneitor/js/app.js
@@ -1,10 +1,8 @@
 var app = {
     canvas:null,
     image:null,
-    /* Víctor: dos nuevos parámetros incluidos */
     imagePreview:null,
     btnCreate: document.querySelector('.create'),
-    /* End Víctor */
     tools:{
         /**
         * This method create a new image with texts, using the html5 canvas.
@@ -19,7 +17,6 @@ var app = {
         * @param {string} properties.imageFormat - Format to image. ex. 'image/png'
         * @returns {string} Return a Base64 DataURI string of the new image
         */
-        /* Víctor: argumento de image cambiado por imagePreview */
         createImage: function (imagePreview, textAbove, textBelow, properties) {
             var canvas = document.createElement('canvas'),
             context = canvas.getContext('2d');
@@ -28,30 +25,21 @@ var app = {
             imagePreview.height > 0 ? canvas.height = imagePreview.height : canvas.height = 300
 
             context.drawImage(imagePreview, 0, 0);
-            /*context.font = properties.textSize +'px impact';
-            context.fillStyle = properties.textColor;
-            context.textAlign = 'center';
-            context.textBaseline = 'hanging';
-            context.strokeStyle = 'black';*/
             
             if (textAbove) {
                 app.tools.drawStroked(context, textAbove, canvas.width / 2, properties.textPadding, properties);
-                /*context.strokeText(textAbove, canvas.width/2, properties.textPadding, 2);
-                context.fillText(textAbove, canvas.width/2, properties.textPadding);*/
             }
             if (textBelow) {
                 app.tools.drawStroked(context, textBelow, canvas.width / 2, canvas.height - properties.textSize - (properties.textPadding / 2), properties);
-                /*context.strokeText(textBelow, canvas.width/2, canvas.height - properties.textSize - properties.textPadding, 2);
-                context.fillText(textBelow, canvas.width/2, canvas.height - properties.textSize - properties.textPadding);*/
             }
 
             return canvas.toDataURL(properties.imageFormat);
         },
 
         create: function () {
-            /* Víctor: He añadido imagePreview y colorText para cambiar el color del texto */
             var image = document.querySelector('#image'),
             imagePreview = document.querySelector('.preview-image'),
+            downloadLink = document.querySelector('#download'),
             textAbove = document.querySelector('.preview-text-top-input').value,
             textBelow = document.querySelector('.preview-text-bottom-input').value,
             colorText = document.querySelector('.preview-text-color-input').value,
@@ -62,16 +50,25 @@ var app = {
                 imageFormat: 'image/png'
             };
 
-            /* Víctor: Como la función createImage nos devuelve la imagen en base64 pues lo cogemos y seteamos el src de la imagen final a la imagen final */
             var dataUrlImage = app.tools.createImage(imagePreview, textAbove.toUpperCase(), textBelow.toUpperCase(), properties);
             if(image) {
                 image.src = dataUrlImage;
             } else {
+                document.querySelector('.img-container').innerHTML = "";
                 document.querySelector('.img-container').innerHTML = '<img id="image" src="' + dataUrlImage + '" />';
             }
+            
+            if(downloadLink) {
+                downloadLink.download = dataUrlImage;
+            } else {
+                document.querySelector('.img-container').innerHTML += '<a id="download" download="meme.png" href="' + dataUrlImage + '" target="_blank">Descargar</a>';
+            }
+            
+            
         },
-
-        /* Victor: Funcionalidad para la preview de la imagen */
+        /**
+         * @description: get every elements which take part in the preview of the meme and add them all actions.
+         */
         previewImage: function() {
             var img = document.querySelector('.preview-image');
             var topPreviewTextInput = document.querySelector('.preview-text-top-input');
@@ -84,28 +81,48 @@ var app = {
             app.tools.previewTextEvent(bottomPreviewTextInput, bottomPreviewText);
             app.tools.previewTextColorEvent(previewTextColorInput, topPreviewText, bottomPreviewText);
         },
+        /**
+         * @description: text written on input is shown in the preview.
+         * @param {object} input - input type text where users write their own text.
+         * @param {object} span - span where text is shown in the preview.
+         */
         previewTextEvent: function(input, span) {
             input.addEventListener("keyup", function() {
                 span.innerText = this.value.toUpperCase();
             });
         },
+        /**
+         * @description: if user change text color on input we get it and we give the color choosed to text.
+         * @param {object} input - input type color where we get the color.
+         * @param {object} textTop - text placed on top of the preview.
+         * @param {object} textBottom - text placed on bottom of the preview.
+         */
         previewTextColorEvent: function(input, textTop, textBottom) {
             input.addEventListener("change", function() {
                 textTop.style.color = this.value;
                 textBottom.style.color = this.value;
             });
         },
+        /**
+         * @description: here we apply styles over text for the final meme.
+         * @param {object} ctx - context (canvas).
+         * @param {string} text - text of the meme.
+         * @param {int} x - horizontal text position.
+         * @param {int} y - vertical text position.
+         * @param {object} properties - properties for the texts.
+         */
         drawStroked: function(ctx, text, x, y, properties) {
             ctx.font = properties.textSize +'px impact';
             ctx.textAlign = 'center';
             ctx.textBaseline = 'hanging';
             ctx.strokeStyle = 'black';
             ctx.lineWidth = 8;
+            ctx.lineJoin="miter";
+            ctx.miterLimit=2;
             ctx.strokeText(text, x, y);
             ctx.fillStyle = properties.textColor;
             ctx.fillText(text, x, y);
         }
-        /* End Victor */
     },
 };
 
