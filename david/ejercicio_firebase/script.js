69a4731f10b9b58830350065f6848d9e5fc12ffe
k0sasp <davidsalazarsalinas@gmail.com>
2018-01-25 23:01:53 +0000

    peliculas final

--- david/ejercicio_firebase/script.js
+++ david/ejercicio_firebase/script.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDWflvEHULO1PxMm4iF0_SF21Ddu1kti-8",
+    apipelicula: "AIzaSyDWflvEHULO1PxMm4iF0_SF21Ddu1kti-8",
     authDomain: "pelis-b3dd4.firebaseapp.com",
     databaseURL: "https://pelis-b3dd4.firebaseio.com",
     projectId: "pelis-b3dd4",
@@ -11,9 +11,11 @@ firebase.initializeApp(config);
 var opciones = {
     init:function () {
         opciones.leer(opciones.mis_pelis);
+        opciones.initButtons();
     },
     mis_pelis : firebase.database().ref(),
     leer: function(referencia){
+        document.querySelector('.ficha-peli').style.display = 'none';
         referencia.on("value", function(snapshot) {
             document.querySelector(".container").innerHTML = "";
             for(var pelicula in snapshot.val()){
@@ -25,19 +27,18 @@ var opciones = {
                     <div class="col-md-4">
                         <div class="row lista">
                             <div class="col-md-6">
-                                <button type="button" data-type="ver" class="btn btn-default">
+                                <button type="button" data-id="${pelicula}" data-type="ver" class="btn btn-default">
                                     Ver
                                 </button>
                             </div>
                             <div class="col-md-6">
-                                <button type="button" data-type="borrar" class="btn btn-default borrar">
+                                <button type="button" data-id="${pelicula}" data-type="borrar" class="btn btn-default borrar">
                                     Borrar
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>`;
-                opciones.initButtons(snapshot.val()[pelicula].Title,snapshot.val()[pelicula].Year,snapshot.val()[pelicula].Poster)
             }
         }, function (errorObject) {
             console.log("Fallo en lectura de datos: " + errorObject.code);
@@ -67,19 +68,31 @@ var opciones = {
         xmlHttp.open("GET", url, true);
         xmlHttp.send();
     },
-    initButtons: function(titulo,anio,img){
-        var botones = document.querySelector(".lista");
+    initButtons: function(){
+        var botones = document.querySelector(".container");
         botones.addEventListener("click",function(e){
             if(e.target.nodeName === 'BUTTON'){
-                if(e.target.parentNode.getAttribute('data-type')==='ver'){
-                    document.querySelector("#titulo strong").innerHTML = titulo;
-                    document.querySelector("#anio strong").innerHTML = anio;
-                    document.querySelector(".ficha-peli").style.display = "block";
-                    document.querySelector(".container-ficha img").src = img;
+                var film_details = opciones.mis_pelis.child(e.target.getAttribute('data-id'));
+                if(e.target.getAttribute('data-type') === "ver"){
+                    opciones.verDetalles(film_details)
+                }else{
+                    opciones.borrarPeli(film_details)
                 }
             }
         });     
-    }   
+    },
+    verDetalles: function(referencia){
+        referencia.once('value', function (snapshot) {
+            document.querySelector('.ficha-peli').style.display = 'block';
+            document.querySelector('#titulo strong').innerHTML=snapshot.val().Title;
+            document.querySelector('#anio strong').innerHTML=snapshot.val().Year;
+            document.querySelector('.caption img').src = snapshot.val().Poster;
+        })
+    },
+    borrarPeli:function (referencia) {
+        document.querySelector('.ficha-peli').style.display = 'none';
+        referencia.remove();
+    }
 }
 
 function runScript(e) {
@@ -90,5 +103,5 @@ function runScript(e) {
         console.log("No te entro por el aro")
     }
 }
-    
+
 opciones.init();
