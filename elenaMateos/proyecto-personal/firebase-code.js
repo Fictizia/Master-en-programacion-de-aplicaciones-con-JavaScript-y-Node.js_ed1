dff4e19b4dcd98679c68d4527fccf98e93d30529
elenamlopez <elena.mateos1@gmail.com>
2018-02-25 14:31:49 +0100

    Added some styles and login button at header

--- elenaMateos/proyecto-personal/firebase-code.js
+++ elenaMateos/proyecto-personal/firebase-code.js
@@ -16,16 +16,17 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   var userData = JSON.stringify(result.user)
       userData = JSON.parse(userData)
       console.log('Los datos del usuario son: ' + userData);
-
   // Alamacenando el usuario en /user/{{uid}}/datos...
-  userRef.child(userData.uid).set(userData)
+  userRef.child(userData.uid).set(userData);
+  // Hace visible el botón de logOut y oculta el de login:
+  githubBtn.classList.toggle('hidden');
+  githubLogOut.classList.toggle('hidden');
 }).catch(function(error) {
   console.log( error + " Error con el login");
 })
 }
 
 function logOut (){
-  
   firebase.auth().signOut().then(function() {
   console.log("User OUT!");
 }).catch(function(error) {
@@ -41,13 +42,10 @@ githubLogOut.addEventListener('click', logOut);
 
 /**
  * Guardar favoitos: El usuario puede guardar datos de sus sitios favoritos
- * 
+ *
  * 1. Crear formulrio de guardar favorito
- * 2. Crear modal para guardar favorito: 
+ * 2. Crear modal para guardar favorito:
  *  2.1. Input de tipo de favorito (casa, trabajo, colegio, casa2, trabajo2...)
  *  2.2. Menú para seleccionar estación.
  *  2.3. Botón de guardar (toma los datos de esa escaión y los guarda en datos de favorito en firebase).
  */
- 
- 
- 
