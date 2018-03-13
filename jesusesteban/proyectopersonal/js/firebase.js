adf2bf72b4a899bbda05123f71d6ef6ff508831b
Jesus <jesusestebansanchez@gmail.com>
2018-03-13 14:26:39 +0000

    COmentarios JS

--- jesusesteban/proyectopersonal/js/firebase.js
+++ jesusesteban/proyectopersonal/js/firebase.js
@@ -11,10 +11,29 @@ var config = {
 firebase.initializeApp(config);
 
 
+var provider = new firebase.auth.GithubAuthProvider();
+
 // LOGIN Button
 document.getElementById("login").addEventListener("click", login);
+
 function login() {
-    console.log("LOGIN");
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    
 }
 
 
@@ -23,3 +42,5 @@ document.getElementById("register").addEventListener("click", register);
 function register() {
     console.log("REGISTER");
 }
+
+
