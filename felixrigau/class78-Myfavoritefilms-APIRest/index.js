71850b85b91969e6770a32e25e123b11f2366051
Felix Rigau <felixrigau88@gmail.com>
2018-03-19 14:59:00 +0000

    Adding films view and route

--- felixrigau/class78-Myfavoritefilms-APIRest/index.js
+++ felixrigau/class78-Myfavoritefilms-APIRest/index.js
@@ -6,7 +6,16 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index')
+    res.render('index', { title: "Fav Films | Gestiona tus filmes favoritos" })
+});
+
+app.get('/films', (req, res) => {
+    let films = [{
+        name: "Band of Brother",
+        year: 2014,
+        director: 'Spilberg'
+    }];
+    res.render('films', { title: "Fav Films | Mis filmes", myFilms: films })
 });
 
 app.listen(process.env.PORT);
