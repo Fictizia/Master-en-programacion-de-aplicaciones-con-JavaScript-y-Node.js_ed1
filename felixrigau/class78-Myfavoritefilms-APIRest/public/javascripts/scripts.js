133eee5f4f06912d1c17712b7ca0ac2735254edd
Felix Rigau <felixrigau88@gmail.com>
2018-03-21 14:27:32 +0000

    The section to select the film is ready

--- felixrigau/class78-Myfavoritefilms-APIRest/public/javascripts/scripts.js
+++ felixrigau/class78-Myfavoritefilms-APIRest/public/javascripts/scripts.js
@@ -4,6 +4,7 @@ APP.urlApi = 'http://www.omdbapi.com/?apikey=b426c167&s=';
 
 APP.start = () => {
   APP.behavior.search();
+  APP.behavior.selectFilm();
 }
 
 APP.behavior = {
@@ -12,27 +13,46 @@ APP.behavior = {
       if (e.keyCode === 13) {
         if (e.target.value != "" && e.target.value != null) {
           let filmName = e.target.value;
-          APP.tools.makeRequest('GET', `${document.URL}getDataAPi/${filmName}`, true, APP.ui.fillSelectFilm);
+          APP.tools.makeRequest('GET', `http://${document.domain}/getDataAPi/${filmName}`, true, APP.ui.fillSelectFilm);
         }
         else {
           console.log('Debes escribir el nombre de una película.')
         }
       }
+      else {
+        if (e.target.value === "") {
+          document.querySelector('.films-list-select').style.height = '0px'
+        }
+      }
+    })
+  },
+
+  selectFilm: () => {
+    document.querySelector('.films-list-select').addEventListener('click', (e) => {
+      if (e.target.nodeName === 'IMG') {
+
+      }
     })
   }
-}
+};
 
 APP.ui = {
   fillSelectFilm: (data) => {
     let listFilms = document.querySelector('.films-list');
+    let selectFilmSection = document.querySelector('.films-list-select');
     listFilms.innerHTML = '';
     if (data) {
       data.Search.forEach((item) => {
         if (item.Poster && item.Poster.indexOf('amazon') !== -1) {
           console.log(item.Title)
-          listFilms.innerHTML += `<li><span>${item.Title} (${item.Year})</span></li>`;
+          listFilms.innerHTML += `<li class="films-list__item">
+            <span><img class="films-list__item-image" src="${item.Poster}" /></span>
+            <span class="films-list__item-title">${item.Title} (${item.Year})</span>
+            <div class="films-list__item-mask"></div>
+          </li>`;
         }
       });
+      selectFilmSection.style.height = '332px';
     }
     else {
       console.log('No hay datos');
