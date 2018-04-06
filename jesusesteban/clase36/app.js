ff7bcccc00a3d76cb9c3e914332a00ca1c91d0cb
Jesus <jesusestebansanchez@gmail.com>
2018-04-06 16:31:33 +0000

    Clase 36

--- jesusesteban/clase36/app.js
+++ jesusesteban/clase36/app.js
@@ -1,28 +1,49 @@
+
+// Creamos un array vacio para añadir todos los contactos que vayamos creando
+var contactos = [];
+
 // ADD CONTACT
-function addContact(){
+function addContact(contacto){
 
 	// Inputs
     var nombre = document.getElementById("name").value,
         telefono = document.getElementById("phone").value,
-        mail = document.getElementById("mail").value,
-        dataTable = document.getElementById("data-table");
+        mail = document.getElementById("mail").value;
 	
-	// Guardamos los datos en localstorage
-	
-	localStorage.setItem('nombre', nombre);
-	localStorage.setItem('telefono', telefono);
-	localStorage.setItem('mail', mail);
-	
-	
-	// Mostramos los datos en el HTML	
-	var dataNombre = localStorage.getItem('nombre'),
-	    dataTelefono = localStorage.getItem('telefono'),
-	    dataMail = localStorage.getItem('mail');
+    // var contacto
+    var contacto = {
+        nombre: nombre,
+        telefono: telefono,
+        mail: mail
+    };
 
-    document.querySelector('.data-nombre').innerHTML = dataNombre;
-    document.querySelector('.data-telefono').innerHTML = dataTelefono;
-    document.querySelector('.data-mail').innerHTML = dataMail;
+	
+    // Guardamos los datos en localstorage
+    if (nombre && telefono && mail) {
+        contactos.push(contacto);
+        localStorage.setItem("contactos",JSON.stringify(contactos));
+        printContact(contacto, contactos.length -1);
+        return true;
+    }else {
+        console.log("NO guarda los datos");
+        return false;
+    }
 
+
+
+}
+
+// Mostramos los datos en el HTML	
+function printContact(contacto, indice){
+
+    var dataTable = document.querySelector(".data-table");
+    dataTable.innerHTML += 
+        "<li class=\"data-list\" data-id=\""+indice+"\">"+
+            "<span class=\"data-nombre\">"+"Nombre: "+contacto.nombre+"</span>"+
+            "<span class=\"data-telefono\">"+"Teléfono: "+contacto.telefono+"</span>"+
+            "<span class=\"data-mail\">"+"Mail: "+contacto.mail+"</span>"+
+            "<button data-delete=\""+indice+"\" type=\"button\" class=\"delete btn btn-danger\">"+ "Borrar Contacto"+"</button>"+
+        "</li>";
 }
 
 document.getElementById("anadir").addEventListener("click", function(){
@@ -30,22 +51,25 @@ document.getElementById("anadir").addEventListener("click", function(){
 });
 
 
-// DELETE CONTACT
+// Borramos un contacto
 function deleteContact(){
-    console.log("Borro un solo contacto");
-    localStorage.removeItem('nombre');
-    localStorage.removeItem('telefono');
-    localStorage.removeItem('mail');
+
+
 }
 
-document.getElementById("delete").addEventListener("click", function(){
+document.querySelector(".data-table").addEventListener("click", function(){
     deleteContact();
 });
 
 
-// DELETE ALL
+// Borramos todos los contactos 
 function deleteAll(){
     localStorage.clear();
+    
+    var deleteAll = document.querySelectorAll('.data-table li');
+    for(var i=0; li=deleteAll[i]; i++) {
+        li.parentNode.removeChild(li);
+    }    
 }
 
 document.getElementById("delete-all").addEventListener("click", function(){
@@ -54,12 +78,9 @@ document.getElementById("delete-all").addEventListener("click", function(){
 
 
 
-// RECOVERY CONTACT
+// Recuperamos un contacto 
 function recoveryContact(){
     console.log("Recuperar contacto");
-    localStorage.getItem('nombre');
-    localStorage.getItem('telefono');
-    localStorage.getItem('mail');
 }
 
 document.getElementById("recovery").addEventListener("click", function(){
